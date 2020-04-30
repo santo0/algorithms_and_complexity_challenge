e014807d5525bf35f800070242317a9e300b8cde
GuillemCamats <guillemcamats@gmail.com>
2020-04-30 15:36:27 +0200

    main arguments i logging

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -1,9 +1,20 @@
-class MedianSample():
-    def __init__(self, id, date, fasta_sequence=""):
-        self.id = id
-        self.date = date
-        self.fasta = fasta_sequence
+import sys
+import optparse
+from optparse import OptionParser
+import os.path
+import logging
 
-# main
-def test():
-    pass
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option('-d','--debug',dest = "debug", action='store_true',default=False,help='debug mode')
+    parser.add_option("-c", "--csv", dest="csv", default = './sequences.csv',help="path of csv file")
+    parser.add_option("-f", "--fasta", dest="fasta", default = './sequences_fasta.fasta', help="path of fasta file")
+    (options, args) = parser.parse_args()
+    if not (os.path.isfile(options.csv) and os.path.isfile(options.fasta)):
+        sys.exit(1)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s',datefmt="%y/%m/%d/-%H:%M:%S",level=logging.INFO)    
+    if options.debug:
+        logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s',datefmt="%y/%m/%d/-%H:%M:%S",level=logging.DEBUG)
+    csv_path = options.csv
+    fasta_path = options.fasta
+    
