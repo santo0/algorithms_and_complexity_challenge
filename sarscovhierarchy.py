c8ef5a7124f37f2f02a8116dcccf627ab8cecd5d
GuillemCamats <guillemcamats@gmail.com>
2020-05-22 13:21:26 +0200

    tree

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -19,6 +19,8 @@ import random
 from preprocessing import get_samples_of_median_length_by_country, get_fasta_sequences
 from sequence_alignement import get_samples_alignement_matrix
 from classify import create_clustering
+import networkx as nx
+
 
 def main():
     '''Get arguments and calls main functions'''
@@ -47,21 +49,37 @@ def main():
     print(score_matrix)
     print("--- %s seconds for getting score matrix ---" %
           (time.time() - start_time))
-    
-    points=[]
-    i=0
-    while i<6:
-        element=random.randrange(0,30,1)
+    final_clusters = clustering(median_sample_list, score_matrix)
+    Tree = nx.Graph()
+    #countries = []
+    #for sample in median_sample_list:
+    #    countries.append(sample.geolocation)
+    #Tree.add_nodes_from(countries)
+    Tree.add_node('Za Warudo')
+    for medoid in final_clusters:
+        Tree.add_node(medoid)
+        Tree.add_edge('Centre', medoid)
+        for country in final_clusters[medoid]:
+            Tree.add_node(country)
+            Tree.add_edge(medoid, country)
+    nx.draw(Tree, with_labels=True, font_size = '6') 
+    plt.show()   
+
+def clustering(median_sample_list, score_matrix):
+    points = []
+    i = 0
+    while i < 6:
+        element = random.randrange(0, 30, 1)
         if element not in points:
             points.append(element)
-            i+=1
+            i += 1
     clustering_with_geolocalitation = {}
-    resultClusters=create_clustering(points,{},score_matrix)
+    resultClusters=create_clustering(points, {}, score_matrix)
     for key in resultClusters.keys():
         clustering_with_geolocalitation.update({median_sample_list[int(key)].geolocation:[]})
         for value in resultClusters[key]:
             clustering_with_geolocalitation[median_sample_list[int(key)].geolocation].append(median_sample_list[value].geolocation)
+    return clustering_with_geolocalitation
 
-    print(clustering_with_geolocalitation.items())
 if __name__ == "__main__":
     main()
