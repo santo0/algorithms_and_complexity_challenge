4e3cdbd1dd459090cf68fc261dd0ecebe064531d
santo0 <lemarti3472@gmail.com>
2020-05-23 17:38:14 +0200

    pylinted (not completed)

--- dec_timer.py
+++ dec_timer.py
@@ -1,8 +1,16 @@
+'''
+    Title: dec_timer.py
+    Author: Guillem Camats Felip, Adria Juve Sanchez, Marti La Rosa Ramos, Xavier Nadal Reales
+    Date: 25-5-2020
+    Code version: 1.0.0
+    Availability: https://github.com/santo0/algorithms_and_complexity_challenge
+'''
+
 import functools
 import time
 
 def timer(func):
-    """Print the runtime of the decorated function"""
+    '''Print the runtime of the wrapped function'''
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         start_time = time.time()
--- preproc_tests.py
+++ preproc_tests.py
@@ -1,11 +1,14 @@
 import unittest
 import random
-from preprocessing import MedianSample, call_get_median, get_samples_of_median_length_by_country, get_fasta_sequences
+from preprocessing import call_get_median
+
 class PreprocTesting(unittest.TestCase):
     def test_get_median(self):
         for _ in range(1, 100):
-            samples_list = [ ('','',random.randint(0,i)) for i in range(random.randint(1, 500))]
-            self.assertEqual(call_get_median(samples_list), sorted(samples_list)[len(samples_list) // 2])
+            samples_list = [('', '', random.randint(0, i))
+                            for i in range(random.randint(1, 500))]
+            self.assertEqual(call_get_median(samples_list),
+                             sorted(samples_list)[len(samples_list) // 2])
 
 
 if __name__ == '__main__':
