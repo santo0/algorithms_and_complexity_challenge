c82e8318d147ca39816b55dd0d919ba6fd33decd
santo0 <lemarti3472@gmail.com>
2020-05-23 14:35:42 +0200

    epic win rapidin

--- clasf_tests.py
+++ clasf_tests.py
@@ -3,9 +3,11 @@ import random
 from classify import clustering, create_clustering
 class ClasfTesting(unittest.TestCase):  
     def test_clasf_1(self):
-        pass
-
-
+        score_matrix = [[1,2,3,4],
+                        [2,1,3,4],
+                        [3,3,1,4],
+                        [4,4,4,1]]
+    
 
 if __name__ == '__main__':
     unittest.main()
--- classify.py
+++ classify.py
@@ -41,14 +41,15 @@ def calculate_min_puntuation(new_cluster, score_matrix):
 
 def create_clustering(points, score_matrix):
     '''Creates clustering'''
-    new_clusters = {str(point): [] for point in points}
+    new_clusters = {point : [] for point in points}
+    print(points,'eeeeeeeeeeeeeeee')
     for i in range(len(score_matrix)):
         new_min = -1
         for point in points:
             if i == point or new_min == -1 or new_min > score_matrix[i][point]:
                 new_min = score_matrix[i][point]
                 closest_point = point
-        new_clusters[str(closest_point)].append(i)
+        new_clusters[closest_point].append(i)
     next_centers = calculate_min_puntuation(new_clusters, score_matrix)
     if all(elem in next_centers for elem in points):
         return new_clusters
@@ -69,8 +70,8 @@ def clustering(median_sample_list, score_matrix):
     result_clusters = create_clustering(points, score_matrix)
     for key in result_clusters.keys():
         clustering_with_geolocalitation.update(
-            {median_sample_list[int(key)].geolocation: []})
+            {median_sample_list[key].geolocation: []})
         for value in result_clusters[key]:
-            clustering_with_geolocalitation[median_sample_list[int(key)].geolocation].append(
+            clustering_with_geolocalitation[median_sample_list[key].geolocation].append(
                 median_sample_list[value].geolocation)
     return clustering_with_geolocalitation
