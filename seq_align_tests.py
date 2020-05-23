b205fc880699ed8d1403b7fa142f791f0500fcde
santo0 <lemarti3472@gmail.com>
2020-05-23 10:27:54 +0200

    starting seq align tests

---
+++ seq_align_tests.py
@@ -0,0 +1,12 @@
+import unittest
+import random
+from preprocessing import MedianSample
+from sequence_alignement import get_samples_alignement_matrix
+
+
+class SeqAlignTesting(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
